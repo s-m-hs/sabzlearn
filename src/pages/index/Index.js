11984879147032294514599335752b26ea5031ef
s-m-hs <161473021+s-m-hs@users.noreply.github.com>
2024-03-26 17:03:07 +0330

    sabzlezrn

---
+++ src/pages/index/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Index.css'
+import Header from '../../components/header/Header'
+
+export default function Index() {
+  return (
+    <div>
+<Header/>
+    </div>
+  )
+}
