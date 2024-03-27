fb373f7d35af0ea7c50913f8e6b8d7160f0d7181
s-m-hs <161473021+s-m-hs@users.noreply.github.com>
2024-03-27 23:34:52 +0330

    sec307

---
+++ src/components/courceDetailBox/CourceDetailBox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './CourceDetailBox.css'
+
+export default function CourceDetailBox({title,text,icon}) {
+  return (
+
+    <div class="col-4">
+    <div class="course-boxes__box">
+      <div class="course-boxes__box-right">
+        <i class={`course-boxes__box-right-icon fas fa-${icon}`}></i>
+      </div>
+      <div class="course-boxes__box-left">
+        <span class="course-boxes__box-left-title">
+          {title} :
+        </span>
+        <span class="course-boxes__box-left--subtitle">
+          {text}
+        </span>
+      </div>
+    </div>
+  </div>
+
+    )
+}
--- src/pages/courseInfo/CourseInfo.js
+++ src/pages/courseInfo/CourseInfo.js
@@ -4,6 +4,7 @@ import TopBar from '../../components/topBar/TopBar'
 import NavBar from '../../components/navBar/NavBar'
 import Footer from '../../components/footer/Footer'
 import Breadcrumb from '../../components/breadcrumb/Breadcrumb'
+import CourceDetailBox from '../../components/courceDetailBox/CourceDetailBox'
 
 export default function CourseInfo() {
   return (
@@ -18,6 +19,71 @@ links={[
 ]
   
 }/>
+    <section class="course-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <a href="#" class="course-info__link">
+              آموزش برنامه نویسی فرانت اند
+            </a>
+            <h1 class="course-info__title">
+              آموزش 20 کتابخانه جاوااسکریپت برای بازار کار
+            </h1>
+            <p class="course-info__text">
+              امروزه کتابخانه‌ها کد نویسی را خیلی آسان و لذت بخش تر کرده اند. به قدری که حتی امروزه هیچ شرکت برنامه نویسی پروژه های خود را با Vanilla Js پیاده سازی نمی کند و همیشه از کتابخانه ها و فریمورک های موجود استفاده می کند. پس شما هم اگه میخواید یک برنامه نویس عالی فرانت اند باشید، باید کتابخانه های کاربردی که در بازار کار استفاده می شوند را به خوبی بلد باشید
+            </p>
+            <div class="course-info__social-media">
+              <a href="#" class="course-info__social-media-item">
+                <i class="fab fa-telegram-plane course-info__icon"></i>
+              </a>
+              <a href="#" class="course-info__social-media-item">
+                <i class="fab fa-twitter course-info__icon"></i>
+              </a>
+              <a href="#" class="course-info__social-media-item">
+                <i class="fab fa-facebook-f course-info__icon"></i>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-6">
+            <video src="" poster="/images/courses/js_project.png" class="course-info__video" controls></video>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <main class="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-8">
+            <div class="course">
+              {/* <!-- Start Course Boxes --> */}
+              <div class="course-boxes">
+                <div class="row">
+
+
+<CourceDetailBox title='وضعیت دوره ' text='به اتمام رسیده ' icon='graduation-cap'/>
+<CourceDetailBox title='وضعیت دوره ' text='به اتمام رسیده ' icon='clock'/>
+<CourceDetailBox title='وضعیت دوره ' text='به اتمام رسیده ' icon='calendar-alt'/>
+<CourceDetailBox title='وضعیت دوره ' text='به اتمام رسیده ' icon='user-alt'/>
+<CourceDetailBox title='وضعیت دوره ' text='به اتمام رسیده ' icon='info-circle'/>
+<CourceDetailBox title='وضعیت دوره ' text='به اتمام رسیده ' icon='fa-play'/>
+
+
+
+
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </main>
+                  
+
+
+
 <Footer/>
 
 </>
